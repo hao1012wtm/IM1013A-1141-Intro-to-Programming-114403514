9ca7fc3a3907aa705060878c61fb41f5a6c2c222
hao1012wtm <lianghao951012@gmail.com>
2025-09-15 13:40:31 +0000

    update

--- week2/temp.c
+++ week2/temp.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 
 int main(void){
-    float numc,numf;
-    printf("Enter the temparature in Celsius\n");
-    scanf("%f",&numc);
-    numf=numc*9/5+32;
-    printf("The temparature in Fahrenheit is %f\n",numf);
+    float tempc,tempf;
+    printf("Please enter the temparature in Celsius\n");
+    scanf("%f",&tempc);
+    tempf=tempc*9/5+32;
+    printf("The temparature in Fahrenheit is %f\n",tempf);
 }
 //end main
