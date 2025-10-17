41cd12f9f44fc8b93ffecd97b42394a67ff8bb0a
hao1012wtm <lianghao951012@gmail.com>
2025-10-17 03:54:32 +0000

    final update

--- 1141_test1/114403514_T1.c
+++ 1141_test1/114403514_T1.c
@@ -3,31 +3,37 @@
 
 int main (void){
     int selection;
+    //Top table menu
     printf("==== Future City Expo Ticket System ====\n");
     printf("1) Option 1 - Single-Order Quotation\n");
     printf("2) Option 2 - Group Total & Installments\n");
     printf("(-1) End Program\n");
     scanf("%d",&selection);
     if(selection==-1){
+        // to end the program
         printf("Select: %d\n",selection);
     }
     while(selection!=-1){
         if(selection==1){
+            //Single-Order Quotation
             int adult,student,ticket;
             float factor1,factor2,total;
             printf("[Option 1] Enter the number of adults (1-20), or -1 to end:\n");
             scanf("%d",&adult);
             if(adult==-1){
+                // to end the program
                 selection=-1;
                 break;
             }
             printf("[Option 1] Enter the number of students (1-20), or -1 to end:\n");
             scanf("%d",&student);
             if(student==-1){
+                // to end the program
                 selection=-1;
                 break;
             }
             ticket=adult+student;
+            // to decide the quantity discount factor
             if(ticket<=5){
                 factor1=1.00;
             }
@@ -46,39 +52,42 @@ int main (void){
             else{
                 factor2=1.00;
             }
-            total=adult*200+student*150;
-            printf("--- Quotation Summary ---\n");
+            total=adult*200+student*150; //compute the total price
+            printf("\n--- Quotation Summary ---\n");
             printf("Adults: %d, Students: %d, Total Tickets: %d\n",adult,student,ticket);
             printf("Base Total:  %.2f\n",total);
             printf("Quantity Discount Factor: %.2f\n",factor1);
-            if(factor2==0.95){
+            if(factor2<1){
                 printf("Multiple-Of-5 Bonus Applied: YES (Factor: %.2f)\n",factor2);
             }
             else{
                 printf("Multiple-Of-5 Bonus Applied: NO (Factor: %.2f)\n",factor2);
             }
-            total=total*factor1*factor2;
+            total=total*factor1*factor2; //compute the final total price(including discount)
             printf("Finale Payable Amount: %.2f\n",total);
         }
         else if (selection==2){
+            // Group Total& Installments
             float principal,period,fee,totalfee,rate,repayment;
             printf("[Option 2] Enter principal in dollars(positive), or -1 to end:\n");
             scanf("%f",&principal);
             if(principal==-1){
+                // to end the program
                 selection=-1;
                 break;
             }
+            // to decide the period,fee,and rate
             else if(principal<500){
                 period=1;
                 fee=0;
                 rate=0;
             }
-            else if(principal<1000){
+            else if(principal<=1000){
                 period=3;
                 fee=20;
                 rate=0.03;
             }
-            else if(principal<2000){
+            else if(principal<=2000){
                 period=6;
                 fee=17;
                 rate=0.025;
@@ -88,8 +97,9 @@ int main (void){
                 fee=12;
                 rate=0.015;
             }
-            repayment=principal*pow(1.00+rate,period);
+            repayment=principal*pow(1.00+rate,period); //compute the repayment
             totalfee=fee*period;
+            printf("\n--- Installment Plan Summary ---\n");
             printf("Principal: %.2f\n",principal);
             printf("Number of Period: %.0f\n",period);
             printf("Handling Fee per Period: %.2f\n",fee);
@@ -103,6 +113,7 @@ int main (void){
         else{
             printf("Select: %d\n",selection);
             printf("Unknown selection. Please choose 1, 2, or -1 to end.\n");
+            //Top table menu
             printf("==== Future City Expo Ticket System ====\n");
             printf("1) Option 1 - Single-Order Quotation\n");
             printf("2) Option 2 - Group Total & Installments\n");
