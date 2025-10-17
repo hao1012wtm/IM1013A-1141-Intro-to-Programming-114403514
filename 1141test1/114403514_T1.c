33b1c3353c847629a05a2bd124ba17cf02721425
hao1012wtm <lianghao951012@gmail.com>
2025-10-17 03:39:07 +0000

    update

--- 1141test1/114403514_T1.c
+++ 1141test1/114403514_T1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <math.h>
+
 int main (void){
     int selection;
     printf("==== Future City Expo Ticket System ====\n");
@@ -59,8 +61,44 @@ int main (void){
             printf("Finale Payable Amount: %.2f\n",total);
         }
         else if (selection==2){
-            continue;
-
+            float principal,period,fee,totalfee,rate,repayment;
+            printf("[Option 2] Enter principal in dollars(positive), or -1 to end:\n");
+            scanf("%f",&principal);
+            if(principal==-1){
+                selection=-1;
+                break;
+            }
+            else if(principal<500){
+                period=1;
+                fee=0;
+                rate=0;
+            }
+            else if(principal<1000){
+                period=3;
+                fee=20;
+                rate=0.03;
+            }
+            else if(principal<2000){
+                period=6;
+                fee=17;
+                rate=0.025;
+            }
+            else{
+                period=12;
+                fee=12;
+                rate=0.015;
+            }
+            repayment=principal*pow(1.00+rate,period);
+            totalfee=fee*period;
+            printf("Principal: %.2f\n",principal);
+            printf("Number of Period: %.0f\n",period);
+            printf("Handling Fee per Period: %.2f\n",fee);
+            printf("Insterest Rate per Period: %.2f\n",rate);
+            printf("Repayment (Compound on Principal): %.2f\n",repayment);
+            printf("Total Handling Fee: %.2f\n",totalfee);
+            repayment=repayment+totalfee;
+            printf("Total Repayment (including Handling Fee): %.2f\n",repayment);
+            printf("Payment per Installment: %.2f\n",repayment/period);
         }
         else{
             printf("Select: %d\n",selection);
